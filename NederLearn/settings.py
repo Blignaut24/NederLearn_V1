caca0b0e2669ac7bb1dafb73a6f5d2d6970e3519
Johann Blignaut <blignaut24@gmail.com>
2024-06-16 08:38:01 +0000

    feat: add registered models

--- NederLearn/settings.py
+++ NederLearn/settings.py
@@ -95,6 +95,8 @@ DATABASES = {
 }
 
 
+CSRF_TRUSTED_ORIGINS = ["https://8000-blignaut24-nederlearn-87ruq8zh7xd.ws-eu114.gitpod.io/",
+    "https://*.herokuapp.com"]
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from.models import Blogpost
+from.models import MediaCategory
+from.models import UserProfile
+from.models import Comment
 
 # Register your models here.
+admin.site.register(Blogpost)
+admin.site.register(MediaCategory)
+admin.site.register(UserProfile)
+admin.site.register(Comment)
