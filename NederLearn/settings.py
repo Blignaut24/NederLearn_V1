4a307cae3c7d55bd6734ec678a64c3053f7fef06
Johann Blignaut <blignaut24@gmail.com>
2024-06-17 20:29:48 +0000

    chore: update csrf_trusted_origins url links

--- NederLearn/settings.py
+++ NederLearn/settings.py
@@ -98,9 +98,11 @@ DATABASES = {
 }
 
 
-CSRF_TRUSTED_ORIGINS = ["https://8000-blignaut24-nederlearn-87ruq8zh7xd.ws-eu114.gitpod.io/", 
-    "https://*.codeanyapp.com",
+CSRF_TRUSTED_ORIGINS = ["https://8000-blignaut24-nederlearn-87ruq8zh7xd.ws-eu114.gitpod.io", 
+    "https://*.gitpod.io",
     "https://*.herokuapp.com"]
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
