dcc0107ab9eb65c3aabea59030347b3ed1370aa7
Johann Blignaut <blignaut24@gmail.com>
2024-06-17 22:20:23 +0000

    fix: correct postlist view with pagination

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,26 @@
 # Standard library imports
-from django.shortcuts import render
-from django.http import HttpResponse
+# from django.shortcuts import render
+from django.views import generic
+from .models import Blogpost, Comment, MediaCategory
 
+# ---------------------
 # Create your views here.
+# ---------------------
+class PostList(generic.ListView):
+    '''
+    This class-based view lists all blogposts, ordered by creation date.
+    Only blogposts with status=1 (published) are shown.
+    Pagination is set to display 6 posts per page.
+    '''
+    # Define the model
+    model = Blogpost
 
+    # Define the queryset
+    queryset = Blogpost.objects.filter(status=1).order_by('created_on')
 
-# Landing page view
-def display_landing_page(request):
-    return HttpResponse("Welcome to the Nederlearn App!")
+    # Define the template
+    template_name = 'index.html'
+
+    # Set pagination
+    paginate_by = 6
+
