6f36eeadde9847a2dea848593eb1aee104f27ef7
Johann Blignaut <blignaut24@gmail.com>
2024-06-17 13:15:24 +0000

    fix[migrations]: Resolved an error with migrations in the blog application
    An error occurred when adding a non-nullable field 'release_year' to the 'blogpost'
    model without specifying a default. The database needed a value to populate existing rows.
    When the migrations were run, it resulted in a 'DuplicateColumn' error as the column 'release_year'
    already existed. This fix resolves the issue by adding a default value to the 'release_year' field to prevent the error during migrations.
    
    On branch main
    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   blog/admin.py
    	modified:   blog/apps.py
    	new file:   blog/migrations/0002_blogpost_release_year_alter_blogpost_content_and_more.py
    	modified:   blog/models.py

--- blog/models.py
+++ blog/models.py
@@ -7,11 +7,13 @@ from django.utils import timezone
 
 current_year = timezone.now().year
 
-
+# ---------------------
+# Define Year Validator
+# ---------------------
 def validate_year(value):
     """
-    A check is made to ensure the 'release year' is between 1800 and 
-    the current year. If the year doesn't fit within this range, an 
+    A check is made to ensure the 'release year' is between 1800 and
+    the current year. If the year doesn't fit within this range, an
     error message will appear.
     """
     current_year = timezone.now().year
@@ -21,16 +23,18 @@ def validate_year(value):
             params={'current_year': current_year},
         )
 
-# Create your models here.
+# ---------------------
+# Define Your Models
+# ---------------------
 STATUS = ((0, 'Draft'), (1, 'Published'))
 
-# MediaCategory
+# MediaCategory Model
 class MediaCategory(models.Model):
     """
-    The MediaCategory model organizes learning materials into 12 unique 
-    classifications, including movies, series, books, music, podcasts, 
-    and European Language levels A1 - C2. Each category is used to 
-    categorize blog posts within the NederLearn app. 
+    The MediaCategory model organizes learning materials into 12 unique
+    classifications, including movies, series, books, music, podcasts,
+    and European Language levels A1 - C2. Each category is used to
+    categorize blog posts within the NederLearn app.
     The 'media_name' field is unique, ensuring no category is replicated.
     """
     media_name = models.CharField(max_length=255, unique=True)
@@ -44,10 +48,10 @@ class MediaCategory(models.Model):
 # Blogpost Model
 class Blogpost(models.Model):
     """
-    The Blogpost model in the NederLearn application captures details like 
-    title, author, creation and update times, and content. It also indicates 
-    the post's status (draft or published), includes an image, media category, 
-    and year of release, and features 'like' and 'bookmark' options for user 
+    The Blogpost model in the NederLearn application captures details like
+    title, author, creation and update times, and content. It also indicates
+    the post's status (draft or published), includes an image, media category,
+    and year of release, and features 'like' and 'bookmark' options for user
     engagement and reference.
     """
     blog_title = models.CharField(max_length=200, unique=True)
@@ -65,7 +69,8 @@ class Blogpost(models.Model):
         'MediaCategory', on_delete=models.SET_NULL,
         related_name='blog_posts', null=True, blank=False
     )
-    release_year = models.IntegerField(validators=[validate_year])
+    # release_year = models.IntegerField(validators=[validate_year])
+    release_year = models.IntegerField(validators=[validate_year], default=current_year) # Possible migration fix
     media_link = models.URLField()
     likes = models.ManyToManyField(
         User, related_name='blogpost_likes', blank=True
@@ -86,18 +91,18 @@ class Blogpost(models.Model):
     def number_of_bookmarks(self):
         return self.bookmarks.count()
 
-# User_Profile Model
+# UserProfile Model
 class UserProfile(models.Model):
     """
-    The UserProfile model is used to collect additional information 
-    about the user that isn't included in the basic User model. 
-    By having a separate UserProfile model, we can keep the User model 
-    (which handles authentication) simpler and more focused, while still 
-    being able to store and retrieve more detailed information about our users. 
-    This information, such as profile picture, location, first language, and 
-    learning goals, can help in personalizing the user's experience and can 
+    The UserProfile model is used to collect additional information
+    about the user that isn't included in the basic User model.
+    By having a separate UserProfile model, we can keep the User model
+    (which handles authentication) simpler and more focused, while still
+    being able to store and retrieve more detailed information about our users.
+    This information, such as profile picture, location, first language, and
+    learning goals, can help in personalizing the user's experience and can
     provide valuable context for certain functionalities in the app.
-    
+
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image = CloudinaryField('image', default='placeholder')
@@ -113,11 +118,10 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-# User Model
 # Comment Model
 class Comment(models.Model):
-    """    
-The "Comment" model is linked to a particular blog post and its author, 
+    """
+The "Comment" model is linked to a particular blog post and its author,
 symbolizing a comment made on that post. It stores:
 
 - The content of the comment
@@ -132,5 +136,3 @@ symbolizing a comment made on that post. It stores:
 
     def __str__(self):
         return f"{self.id} by {self.user.username}"
-
-# Category Model 
