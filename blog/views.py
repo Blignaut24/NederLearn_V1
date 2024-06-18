a9847e1b60d5f7b6a2660e2b753ae49636112996
Johann Blignaut <blignaut24@gmail.com>
2024-06-18 13:29:54 +0000

    chore: update views.py, base.html, and index.html

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@
 # Django Imports
 # ---------------------
 # Standard library imports
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Blogpost, Comment, MediaCategory
 
 # ---------------------
@@ -12,7 +13,6 @@ from .models import Blogpost, Comment, MediaCategory
 # Only blogposts with status=1 (published) are shown.
 # Pagination is set to display 6 posts per page.
 class BlogPostList(generic.ListView):
-
     # Define the model
     model = Blogpost
 
@@ -25,3 +25,37 @@ class BlogPostList(generic.ListView):
 
     # Set pagination
     paginate_by = 6
+
+
+# ---------------------
+# BlogPostDetail View
+# ---------------------
+# This class-based view displays the details of a single blogpost specified by its slug.
+# It also fetches the comments associated with the blogpost and checks if the user has liked the blogpost.
+class BlogPostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        # Define the queryset
+        queryset = Blogpost.objects.filter(status=1)
+
+        # Get the blogpost or return a 404 error
+        blogpost = get_object_or_404(queryset, slug=slug)
+
+        # Get the comments related to the blogpost, order them by creation date and filter out unapproved comments
+        comments = blogpost.comments.filter(approved=True).order_by("-created_on")
+
+        # Check if the user has liked the blogpost
+        liked = False
+        if blogpost.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        # Render the blogpost detail view
+        return render(
+            request,
+            "blogpost_detail.html",
+            {
+                "blogpost": blogpost,
+                "comments": comments,
+                "liked": liked
+            },
+        )
